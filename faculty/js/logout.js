b04e6fc203542c6c63d1557628905c9ededbd9f0
Duethy Elsa Mathews <77284995+Duethyem02@users.noreply.github.com>
2024-04-15 23:25:44 +0530

    Add files via upload

---
+++ faculty/js/logout.js
@@ -0,0 +1,31 @@
+// Get the logout button element
+const logoutButton = document.getElementById('confirm-logout');
+const nologoutButton = document.getElementById('no-logout');
+
+logoutButton.addEventListener('click', () => {
+    fetch('logout.php')
+    .then(response => response.text())
+    .then(data => {
+        if (data === 'success') {
+            document.querySelector('.logout-container').style.display = 'none';
+            // Display the success message
+           showSuccessMessage();
+
+            // Redirect after a delay (adjust as needed)
+            setTimeout(() => {
+                window.location.href = '../Front Page/frontpage.html'; // Adjust the actual login page URL
+            }, 2000); // Redirect after 2 seconds (customize the delay)
+        }
+    })
+    .catch(error => console.error('Error during logout:', error)); // Redirect after 2 seconds (customize the delay)
+});
+
+function showSuccessMessage() {
+    const successMessage = document.querySelector('.success-message');
+    successMessage.textContent = 'You have successfully logged out.\n Redirecting to login page...';
+    successMessage.style.display = 'block'; // Show the message
+}
+
+nologoutButton.addEventListener('click', () => {
+    window.location.href = 'fhome.php';
+});
